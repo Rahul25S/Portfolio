4334c92bc9a8bbfa33b204c3d0a50d6795b81407
Rahul25S <rahul11022004@gmail.com>
2026-05-22 22:22:52 +0530

    created project

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,77 +1,82 @@
 import React, { useState } from "react";
-import { FaHome, FaUser, FaEnvelope, FaLaptopCode } from "react-icons/fa";
+import {
+  FaHome,
+  FaUser,
+  FaEnvelope,
+  FaLaptopCode,
+  FaBars,
+} from "react-icons/fa";
 import { IoMdPhotos } from "react-icons/io";
 import { Link } from "react-scroll";
 
-function NavBar() {
+const NavBar = () => {
   const [nav, setNav] = useState(false);
+
+  const links = [
+    {
+      id: 1,
+      name: "Home",
+      to: "home",
+      icon: <FaHome />,
+    },
+    {
+      id: 2,
+      name: "Project",
+      to: "portfolio",
+      icon: <IoMdPhotos />,
+    },
+    {
+      id: 3,
+      name: "Skills",
+      to: "skills",
+      icon: <FaLaptopCode />,
+      offset: -80,
+    },
+    {
+      id: 4,
+      name: "About",
+      to: "about",
+      icon: <FaUser />,
+    },
+    {
+      id: 5,
+      name: "Contact",
+      to: "contact",
+      icon: <FaEnvelope />,
+    },
+  ];
 
   return (
     <nav className="bg-black p-4 text-xl flex justify-between items-center fixed w-full top-0 z-10">
+      {/* Logo */}
       <div className="text-white text-4xl font-signature">Rahul S</div>
-      <div className="hidden md:flex space-x-4">
-        <Link
-          to="home"
-          smooth
-          duration={500}
-          className="group flex items-center text-white space-x-2 hover:text-blue-500 cursor-pointer"
-        >
-          <FaHome className="transition duration-300 group-hover:translate-x-1" />
-          <span className="opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition duration-300">
-            Home
-          </span>
-        </Link>
-        <Link
-          to="portfolio"
-          smooth
-          duration={500}
-          className="group flex items-center text-white space-x-2 hover:text-blue-500 cursor-pointer"
-        >
-          <IoMdPhotos className="transition duration-300 group-hover:translate-x-1" />
-          <span className="opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition duration-300">
-            Project
-          </span>
-        </Link>
-        <Link
-          to="skills"
-          smooth
-          duration={500}
-          offset={-80}
-          className="group flex items-center text-white space-x-2 hover:text-blue-500 cursor-pointer"
-        >
-          <FaLaptopCode className="transition duration-300 group-hover:translate-x-1" />
-          <span className="opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition duration-300">
-            Skills
-          </span>
-        </Link>
-        <Link
-          to="about"
-          smooth
-          duration={500}
-          className="group flex items-center text-white space-x-2 hover:text-blue-500 cursor-pointer"
-        >
-          <FaUser className="transition duration-300 group-hover:translate-x-1" />
-          <span className="opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition duration-300">
-            About
-          </span>
-        </Link>
 
-        <Link
-          to="contact"
-          smooth
-          duration={500}
-          className="group flex items-center text-white space-x-2 hover:text-blue-500 cursor-pointer"
-        >
-          <FaEnvelope className="transition duration-300 group-hover:translate-x-1" />
-          <span className="opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition duration-300">
-            Contact
-          </span>
-        </Link>
+      {/* Desktop Menu */}
+      <div className="hidden md:flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.id}
+            to={link.to}
+            smooth
+            duration={500}
+            offset={link.offset || 0}
+            className="group flex items-center text-white space-x-2 hover:text-blue-500 cursor-pointer"
+          >
+            <span className="transition duration-300 group-hover:translate-x-1">
+              {link.icon}
+            </span>
+
+            <span className="opacity-0 group-hover:opacity-100 group-hover:translate-x-1 transition duration-300">
+              {link.name}
+            </span>
+          </Link>
+        ))}
       </div>
 
+      {/* Mobile Menu Button */}
       <div className="md:hidden">
         <button onClick={() => setNav(!nav)} className="text-white">
-          <i className="fa fa-bars text-2xl"></i>
+          <FaBars className="text-2xl" />
         </button>
       </div>
 
@@ -81,56 +86,24 @@ function NavBar() {
           nav ? "translate-x-0" : "-translate-x-full"
         } transition-transform duration-300 ease-in-out md:hidden`}
       >
-        <div className="flex flex-col items-center justify-center min-h-screen space-y-4 text-white">
-          <Link
-            to="home"
-            smooth
-            duration={500}
-            className="text-3xl"
-            onClick={() => setNav(false)}
-          >
-            Home
-          </Link>
-          <Link
-            to="about"
-            smooth
-            duration={500}
-            className="text-3xl"
-            onClick={() => setNav(false)}
-          >
-            About
-          </Link>
-          <Link
-            to="skills"
-            smooth
-            duration={500}
-            className="text-3xl"
-            onClick={() => setNav(false)}
-          >
-            Skills
-          </Link>
-          <Link
-            to="portfolio"
-            smooth
-            duration={500}
-            className="text-3xl"
-            onClick={() => setNav(false)}
-          >
-            Portfolio
-          </Link>
-          <Link
-            to="contact"
-            smooth
-            duration={500}
-            className="text-3xl"
-            onClick={() => setNav(false)}
-          >
-            Contact
-          </Link>
+        <div className="flex flex-col items-center justify-center min-h-screen space-y-6 text-white">
+          {links.map((link) => (
+            <Link
+              key={link.id}
+              to={link.to}
+              smooth
+              duration={500}
+              offset={link.offset || 0}
+              className="text-3xl cursor-pointer"
+              onClick={() => setNav(false)}
+            >
+              {link.name}
+            </Link>
+          ))}
         </div>
       </div>
     </nav>
   );
-}
+};
 
 export default NavBar;
